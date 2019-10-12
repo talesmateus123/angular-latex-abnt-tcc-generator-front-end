8e6b9cd117465ba03c9b788232570e4155ff7491
Tales Ferreira <talesmateus1999@hotmail.com>
2019-10-12 02:19:09 -0300

    crud completed

--- src/app/components/document/document-update/document-update.component.ts
+++ src/app/components/document/document-update/document-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Document } from 'src/app/models/document';
+import { DocumentService } from 'src/app/services/document.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-document-update',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-update.component.css']
 })
 export class DocumentUpdateComponent implements OnInit {
-
-  constructor() { }
+  private document:Document
+  private id:string
+  constructor(private documentService:DocumentService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id')
+    this.documentService.getDocument(this.id).subscribe(
+      res => {
+        this.document = res
+      }
+    )
   }
+  update(){
+    this.documentService.updateDocument(this.id, this.document).subscribe(
+      res => {
 
+      }
+    )
+  }
+  cancel(){
+    this.router.navigate(['/'])
+  }
 }

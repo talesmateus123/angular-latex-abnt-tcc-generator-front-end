9e0ba0e9b7936000c75566d8c75c0b9ce96e9c7d
Tales Ferreira <talesmateus1999@hotmail.com>
2019-10-12 02:00:59 -0300

    crud completado

--- src/app/components/document/document-delete/document-delete.component.ts
+++ src/app/components/document/document-delete/document-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Document } from 'src/app/models/document';
+import { DocumentService } from 'src/app/services/document.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-document-delete',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-delete.component.css']
 })
 export class DocumentDeleteComponent implements OnInit {
-
-  constructor() { }
+  id: string
+  document:Document
+  constructor(private documentService:DocumentService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id')
+    this.documentService.getDocument(this.id).subscribe(
+      res => {
+        this.document = res
+      }
+    )
   }
-
+  delete(){
+    this.documentService.deleteDocument(this.id).subscribe(
+      res => {
+        alert('Removido com sucesso!')
+        this.cancel()
+      }
+    )
+  }
+  cancel(){
+    this.router.navigate(['/'])
+  }
 }
--- src/app/components/person/person-delete/person-delete.component.ts
+++ src/app/components/person/person-delete/person-delete.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from 'src/app/models/person';
+import { PersonService } from 'src/app/services/person.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-person-delete',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./person-delete.component.css']
 })
 export class PersonDeleteComponent implements OnInit {
-
-  constructor() { }
+  id: string
+  person:Person
+  constructor(private personService:PersonService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id')
+    this.personService.getPerson(this.id).subscribe(
+      res => {
+        this.person = res
+      }
+    )
   }
-
+  delete(){
+    this.personService.deletePerson(this.id).subscribe(
+      res => {
+        alert('Removido com sucesso!')
+        this.cancel()
+      }
+    )
+  }
+  cancel(){
+    this.router.navigate(['/'])
+  }
 }
